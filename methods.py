e78aced331de23ab143570a02df7623a5d608909
greendar <dwgreen@hwdsb.on.ca>
2025-12-10 09:18:29 -0500

    working on date

--- methods.py
+++ methods.py
@@ -8,3 +8,25 @@ class Player:
         self.name = name
         self.score = startingScore
         self.votesList = []
+
+
+class SnowDay:   
+    def __init__(self, date):
+        """
+        Docstring for __init__       
+        :param date: Date format "2025-12-10"
+        """
+        from datetime import datetime
+        pass
+
+
+
+'''from datetime import datetime
+
+# Example date
+date_str = "2025-12-10"
+date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+
+# Get day of the week
+day_of_week = date_obj.strftime("%A")
+print(day_of_week)  # Output: Wednesday'''
