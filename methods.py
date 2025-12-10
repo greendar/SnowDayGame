9323005dbbab8ff66acd5b9b1a6a20661a76b15d
Mr Green <dwgreen@hwdsb.on.ca>
2025-12-10 07:41:54 -0500

    player class

--- methods.py
+++ methods.py
@@ -1,2 +1,10 @@
 #methods.py
+startingScore = 10
 
+
+class Player:
+    def __init__(self, email, name):
+        self.email = email
+        self.name = name
+        self.score = startingScore
+        self.votesList = []
